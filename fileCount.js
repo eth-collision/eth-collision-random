a012c17351069cd66df5645572295cbdfa1babcd
smallyunet <dev.wangyu@gmail.com>
2023-02-05 11:03:27 +0800

    delete word

--- fileCount.js
+++ fileCount.js
@@ -32,19 +32,12 @@ function getFileCount(file) {
 function countFile() {
   let msg = "";
   Promise.all([
-    getFileCount("no-random.txt").then((res) => (msg += `no-random: ${res}\n`)),
+    getFileCount("no-random.txt").then((res) => (msg += `No: ${res}\n`)),
     getFileCount("yes-random.txt").then(
-      (res) => (msg += `yes-random: ${res}\n`)
+      (res) => (msg += `Yes: ${res}\n`)
     ),
     getFileCount("err-random.txt").then(
-      (res) => (msg += `err-random: ${res}\n`)
-    ),
-    getFileCount("no-random-word.txt").then((res) => (msg += `no-random: ${res}\n`)),
-    getFileCount("yes-random.txt").then(
-      (res) => (msg += `yes-random-word: ${res}\n`)
-    ),
-    getFileCount("err-random-word.txt").then(
-      (res) => (msg += `err-random: ${res}\n`)
+      (res) => (msg += `Err: ${res}\n`)
     ),
   ]).then(() => {
     sendTgMsg(msg);
